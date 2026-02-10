6bbf995cde2206bfe66722186a8a5340491db409
Jake's rpi <jakelamers931@gmail.com>
2026-02-09 20:59:24 -0600

    added basic html to pdf send to sn crontab

---
+++ html_to_pdf.py
@@ -0,0 +1,17 @@
+from weasyprint import HTML
+
+from datetime import datetime
+
+def html_to_pdf(body="")
+    """
+    TODO
+    """
+    #Test Body should create dynamically
+    body = """
+    <html>
+        <head></head>
+        Hello World!
+    </html>
+    """
+
+    HTML(string=body).write_pdf(f"{datetime.today().strftime('%Y-%m-%d')}.pdf")
